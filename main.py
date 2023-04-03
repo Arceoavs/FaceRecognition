ccb8fc9c3b4da342f6077f8314724193d5324df9
Arnold Arz <arnoarz99@gmail.com>
2023-04-03 17:07:14 +0200

    🎨

--- main.py
+++ main.py
@@ -108,30 +108,44 @@ class IdData:
         print()
 
     def find_matching_ids(self, embs):
-        if self.id_names:
-            matching_ids = []
-            matching_distances = []
-            distance_matrix = pairwise_distances(embs, self.embeddings)
-            for distance_row in distance_matrix:
-                min_index = np.argmin(distance_row)
-                if distance_row[min_index] < self.distance_treshold:
-                    matching_ids.append(self.id_names[min_index])
-                    matching_distances.append(distance_row[min_index])
-                else:
-                    matching_ids.append(None)
-                    matching_distances.append(None)
-        else:
-            matching_ids = [None] * len(embs)
-            matching_distances = [np.inf] * len(embs)
-        return matching_ids, matching_distances
+        # Calculate the distance matrix between the input embeddings and the saved embeddings.
+        distance_matrix = pairwise_distances(embs, self.embeddings)
+
+        # Find the matching ID and distance for each row in the distance matrix.
+        # Store the results as a list of tuples.
+        # If self.id_names is None, set the ID to None and the distance to infinity for all rows.
+        matching_results = (
+            [
+                (
+                    self.id_names[np.argmin(distance_row)],
+                    np.min(distance_row)
+                    if np.min(distance_row) < self.distance_treshold
+                    else np.inf,
+                )
+                for distance_row in distance_matrix
+            ]
+            if self.id_names
+            else [(None, np.inf)] * len(embs)
+        )
+
+        # Unpack the results into two lists: matching_ids and matching_distances.
+        # If self.id_names is None, return (None, [np.inf] * len(embs)).
+        # Otherwise, return the two lists as a tuple.
+        return zip(*matching_results) if self.id_names else (None, [np.inf] * len(embs))
 
 
 def load_model(model):
+    """Load the model from the given path."""
+
+    # Expand the user path and check if the given model is a file.
     model_exp = os.path.expanduser(model)
     if not os.path.isfile(model_exp):
         raise ValueError("Specify model file, not directory!")
 
+    # Print the path of the model file being loaded.
     print(f"Loading model filename: {model_exp}")
+
+    # Load the graph definition from the model file and import it.
     with tf.io.gfile.GFile(model_exp, "rb") as f:
         graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
@@ -139,164 +153,151 @@ def load_model(model):
 
 
 def main(args):
-    with tf.Graph().as_default():
-        with tf.compat.v1.Session() as sess:
-            # Setup models
-            mtcnn = detect_and_align.create_mtcnn(sess, None)
+    with tf.Graph().as_default(), tf.compat.v1.Session() as sess:
+        # Setup models
+        mtcnn = detect_and_align.create_mtcnn(sess, None)
+        load_model(args.model)
+        get_tensor = tf.compat.v1.get_default_graph().get_tensor_by_name
+        images_placeholder = get_tensor("input:0")
+        embeddings = get_tensor("embeddings:0")
+        phase_train_placeholder = get_tensor("phase_train:0")
 
-            load_model(args.model)
-            images_placeholder = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                "input:0"
-            )
-            embeddings = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                "embeddings:0"
-            )
-            phase_train_placeholder = (
-                tf.compat.v1.get_default_graph().get_tensor_by_name("phase_train:0")
-            )
+        # Load anchor IDs
+        id_data = IdData(
+            args.id_folder[0],
+            mtcnn,
+            sess,
+            embeddings,
+            images_placeholder,
+            phase_train_placeholder,
+            args.threshold,
+        )
 
-            # Load anchor IDs
-            id_data = IdData(
-                args.id_folder[0],
-                mtcnn,
-                sess,
-                embeddings,
-                images_placeholder,
-                phase_train_placeholder,
-                args.threshold,
-            )
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FPS, 1)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-            cap = cv2.VideoCapture(0)
-            cap.set(cv2.CAP_PROP_FPS, 0.1)
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-            frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        show_options = {"landmarks": False, "bb": False, "id": True, "fps": False}
+        frame_detections = None
 
-            show_landmarks = False
-            show_bb = False
-            show_id = True
-            show_fps = False
-            frame_detections = None
-            while True:
-                start = time.time()
-                _, frame = cap.read()
+        while True:
+            start = time.time()
+            _, frame = cap.read()
 
-                # Locate faces and landmarks in frame
-                (
-                    face_patches,
+            # Locate faces and landmarks in frame
+            (
+                face_patches,
+                padded_bounding_boxes,
+                landmarks,
+            ) = detect_and_align.detect_faces(frame, mtcnn)
+
+            if len(face_patches) > 0:
+                face_patches = np.stack(face_patches)
+                feed_dict = {
+                    images_placeholder: face_patches,
+                    phase_train_placeholder: False,
+                }
+                embs = sess.run(embeddings, feed_dict=feed_dict)
+
+                matching_ids, matching_distances = id_data.find_matching_ids(embs)
+                frame_detections = {
+                    "embs": embs,
+                    "bbs": padded_bounding_boxes,
+                    "frame": frame.copy(),
+                }
+
+                print("Matches in frame:")
+                for bb, landmark, matching_id, dist in zip(
                     padded_bounding_boxes,
                     landmarks,
-                ) = detect_and_align.detect_faces(frame, mtcnn)
+                    matching_ids,
+                    matching_distances,
+                ):
+                    if matching_id is None:
+                        matching_id = "Unknown"
+                        print("Unknown! Couldn't fint match.")
+                    else:
+                        print("Hi %s! Distance: %1.4f" % (matching_id, dist))
 
-                if len(face_patches) > 0:
-                    face_patches = np.stack(face_patches)
-                    feed_dict = {
-                        images_placeholder: face_patches,
-                        phase_train_placeholder: False,
-                    }
-                    embs = sess.run(embeddings, feed_dict=feed_dict)
-
-                    matching_ids, matching_distances = id_data.find_matching_ids(embs)
-                    frame_detections = {
-                        "embs": embs,
-                        "bbs": padded_bounding_boxes,
-                        "frame": frame.copy(),
-                    }
-
-                    print("Matches in frame:")
-                    for bb, landmark, matching_id, dist in zip(
-                        padded_bounding_boxes,
-                        landmarks,
-                        matching_ids,
-                        matching_distances,
-                    ):
-                        if matching_id is None:
-                            matching_id = "Unknown"
-                            print("Unknown! Couldn't fint match.")
-                        else:
-                            print("Hi %s! Distance: %1.4f" % (matching_id, dist))
-
-                        if show_id:
-                            font = cv2.FONT_HERSHEY_SIMPLEX
-                            cv2.putText(
-                                frame,
-                                matching_id,
-                                (bb[0], bb[3]),
-                                font,
-                                1,
-                                (255, 255, 255),
-                                1,
-                                cv2.LINE_AA,
-                            )
-                        if show_bb:
-                            cv2.rectangle(
-                                frame, (bb[0], bb[1]), (bb[2], bb[3]), (255, 0, 0), 2
-                            )
-                        if show_landmarks:
-                            for j in range(5):
-                                size = 1
-                                top_left = (
-                                    int(landmark[j]) - size,
-                                    int(landmark[j + 5]) - size,
-                                )
-                                bottom_right = (
-                                    int(landmark[j]) + size,
-                                    int(landmark[j + 5]) + size,
-                                )
-                                cv2.rectangle(
-                                    frame, top_left, bottom_right, (255, 0, 255), 2
-                                )
-                else:
-                    print("Couldn't find a face")
-
-                end = time.time()
-
-                seconds = end - start
-                fps = round(1 / seconds, 2)
-
-                if show_fps:
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(
-                        frame,
-                        str(fps),
-                        (0, int(frame_height) - 5),
-                        font,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA,
-                    )
-
-                cv2.imshow("frame", frame)
-
-                key = cv2.waitKey(1)
-                if key == ord("q"):
-                    break
-                elif key == ord("l"):
-                    show_landmarks = not show_landmarks
-                elif key == ord("b"):
-                    show_bb = not show_bb
-                elif key == ord("i"):
-                    show_id = not show_id
-                elif key == ord("f"):
-                    show_fps = not show_fps
-                elif key == ord("s") and frame_detections is not None:
-                    for emb, bb in zip(
-                        frame_detections["embs"], frame_detections["bbs"]
-                    ):
-                        patch = frame_detections["frame"][
-                            bb[1] : bb[3], bb[0] : bb[2], :
-                        ]
-                        cv2.imshow("frame", patch)
-                        cv2.waitKey(1)
-                        new_id = easygui.enterbox(
-                            "Who's in the image? Leave empty for non-valid"
+                    if show_options["id"]:
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        cv2.putText(
+                            frame,
+                            matching_id,
+                            (bb[0], bb[3]),
+                            font,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA,
                         )
-                        if len(new_id) > 0:
-                            id_data.add_id(emb, new_id, patch)
+                    if show_options["bb"]:
+                        cv2.rectangle(
+                            frame, (bb[0], bb[1]), (bb[2], bb[3]), (255, 0, 0), 2
+                        )
+                    if show_options["landmarks"]:
+                        for j in range(5):
+                            size = 1
+                            top_left = (
+                                int(landmark[j]) - size,
+                                int(landmark[j + 5]) - size,
+                            )
+                            bottom_right = (
+                                int(landmark[j]) + size,
+                                int(landmark[j + 5]) + size,
+                            )
+                            cv2.rectangle(
+                                frame, top_left, bottom_right, (255, 0, 255), 2
+                            )
+            else:
+                print("Couldn't find a face")
 
-            cap.release()
-            cv2.destroyAllWindows()
+            end = time.time()
+
+            seconds = end - start
+            fps = round(1 / seconds, 2)
+
+            if show_options["fps"]:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(
+                    frame,
+                    str(fps),
+                    (0, int(frame_height) - 5),
+                    font,
+                    1,
+                    (255, 255, 255),
+                    1,
+                    cv2.LINE_AA,
+                )
+
+            cv2.imshow("frame", frame)
+
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                break
+            elif key == ord("l"):
+                show_options["landmarks"] = not show_options["landmarks"]
+            elif key == ord("b"):
+                show_options["bb"] = not show_options["bb"]
+            elif key == ord("i"):
+                show_options["id"] = not show_options["id"]
+            elif key == ord("f"):
+                show_options["fps"] = not show_options["fps"]
+            elif key == ord("s") and frame_detections is not None:
+                for emb, bb in zip(frame_detections["embs"], frame_detections["bbs"]):
+                    patch = frame_detections["frame"][bb[1] : bb[3], bb[0] : bb[2], :]
+                    cv2.imshow("frame", patch)
+                    cv2.waitKey(1)
+                    new_id = easygui.enterbox(
+                        "Who's in the image? Leave empty for non-valid"
+                    )
+                    if len(new_id) > 0:
+                        id_data.add_id(emb, new_id, patch)
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
